9fe5d02384500c7811dcf152bcd1f2ce1b570617
plasmafelipe <plasmafelipe@gmail.com>
2026-05-05 23:02:52 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,5 @@
-// COLE AQUI OS DADOS DO SEU PROJETO SUPABASE
-// Supabase > Project Settings > API
+// COLE AQUI OS DADOS DO SEU SUPABASE
+// Vá no Supabase: Project Settings > API
 
-const SUPABASE_URL = "COLE_SUA_URL_AQUI";
-const SUPABASE_ANON_KEY = "COLE_SUA_CHAVE_ANON_PUBLIC_AQUI";
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = "qdglqfjhncjmkfoowqxu.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
