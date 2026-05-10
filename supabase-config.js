972203eeff8a21aa7b7480ed6d01254acec22c5f
plasmafelipe <plasmafelipe@gmail.com>
2026-05-09 22:55:49 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -2,4 +2,4 @@
 // Vá no Supabase: Project Settings > API
 
 const SUPABASE_URL = "https://qdglqfjhncjmkfoowqxu.supabase.co";
-const SUPABASE_ANON_KEY = "https://sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
+const SUPABASE_ANON_KEY = "sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
