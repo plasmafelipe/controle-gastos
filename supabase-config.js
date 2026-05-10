4cf0284491fccbc6b5e943abd4674e3556c10abe
plasmafelipe <plasmafelipe@gmail.com>
2026-05-09 23:35:04 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,6 +2,3 @@
 // Exemplo:
 // const SUPABASE_URL = "https://qdglqfjhncjmkfoowqxu.supabase.co";";
 // const SUPABASE_ANON_KEY = "sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
-
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_PROJETO";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_PUBLISHABLE_KEY";
