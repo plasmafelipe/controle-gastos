a8dc386c7297c09112d2d12e1c013359a5980558
minhasfinancasapp <plasmafelipe@gmail.com>
2026-05-11 23:10:44 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,3 @@
-// COLE AQUI OS DADOS DO SEU SUPABASE
-// Exemplo:
-// const SUPABASE_URL = "https://seuprojeto.supabase.co";
-// const SUPABASE_ANON_KEY = "sua_publishable_key";
+const SUPABASE_URL = "https://qdglqfjhncjmkfoowqxu.supabase.co";
 
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_PROJETO";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_PUBLISHABLE_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
