2e3141a7748fd98d317004876a89d5e7d463990c
minhasfinancasapp <plasmafelipe@gmail.com>
2026-05-12 21:46:59 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,2 @@
 const SUPABASE_URL = "https://qdglqfjhncjmkfoowqxu.supabase.co";
-
-const SUPABASE_ANON_KEY = "SUA_CHAVE_PUBLISHABLE_AQUI";
+const SUPABASE_ANON_KEY = "sb_publishable_o17TPoqjrd-IkZ3lShf21g_hv2SBxQh";
